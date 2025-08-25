4ed25f59b221dbf3328ff23d2610241f1d8d3efb
bishwa <108havocx108@gmail.com>
2025-08-25 19:34:29 +0545

    update the persistent method as well as added new states and actions

--- src/app/stores/todo-store.tsx
+++ src/app/stores/todo-store.tsx
@@ -1,48 +1,76 @@
-import { todo } from "node:test";
-import React from "react";
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
 
 //for a single to-do item.
-type Todo = {
+export type Todo = {
   id: number;
   text: string;
   completed: boolean;
 };
+
 // for the entire state of a to-do app
 type TodoStoreState = {
   todos: Todo[];
+  isFormOpen: boolean;
+  openForm: () => void;
+  closeForm: () => void;
   addTodo: (todo: Todo) => void;
+  updateTodo: (todoId: number, newText: string) => void;
   removeTodo: (todoId: number) => void;
   toggleTodoStatus: (todoId: number) => void;
 };
 
-const TodoStore = create<TodoStoreState>((set) => ({
-  //initial state of the todos
-  todos: [],
+const useTodoStore = create<TodoStoreState>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        //initial state of the todos
+        todos: [],
+        isFormOpen: false,
 
-  //action for adding new todo
-  addTodo: (todo) => {
-    set((state) => ({
-      todos: [todo, ...state.todos],
-    }));
-  },
+        //actions for form open and close
+        openForm: () => set({ isFormOpen: true }),
+        closeForm: () => set({ isFormOpen: false }),
 
-  //action to remove the todo item by Id.
-  removeTodo: (todoId) => {
-    set((state) => ({
-      todos: state.todos.filter((todo) => todo.id !== todoId),
-    }));
-  },
+        //action for adding new todo
+        addTodo: (todo) => {
+          set((state) => ({
+            todos: [todo, ...state.todos],
+            isFormOpen: false,
+          }));
+        },
 
-  //action to toggle the todo status
-  toggleTodoStatus: (todoId) => {
-    set((state) => ({
-      todos: state.todos.map((todo) =>
-        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
-      ),
-    }));
-  },
-}));
+        updateTodo: (todoId, newText) => {
+          set((state) => ({
+            todos: state.todos.map((todo) =>
+              todo.id === todoId ? { ...todo, text: newText } : todo
+            ),
+          }));
+        },
 
-export default TodoStore;
+        //action to remove the todo item by Id.
+        removeTodo: (todoId) => {
+          set((state) => ({
+            todos: state.todos.filter((todo) => todo.id !== todoId),
+          }));
+        },
+
+        //action to toggle the todo status
+        toggleTodoStatus: (todoId) => {
+          set((state) => ({
+            todos: state.todos.map((todo) =>
+              todo.id === todoId
+                ? { ...todo, completed: !todo.completed }
+                : todo
+            ),
+          }));
+        },
+      }),
+      {
+        name: "Todo list", // name of the item in local sorage, see in devTools
+      }
+    )
+  )
+);
+
+export default useTodoStore;
