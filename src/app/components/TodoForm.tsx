479e8dc796b8a7c415f4efbc5fb1b21c5b40e8cc
bishwa <108havocx108@gmail.com>
2025-08-29 19:42:18 +0545

    updated the code logic added filter logics and rendered todolist component

--- src/app/components/TodoForm.tsx
+++ src/app/components/TodoForm.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import useTodoStore from "../stores/todo-store";
 import DialogueBox from "./Dialaogue-box";
 import { AnimatePresence } from "motion/react";
+import TodoList from "./Todo-list";
 
 const TodoForm = () => {
-  const addTodo = useTodoStore((state) => state.addTodo);
-
   const isFormOpen = useTodoStore((state) => state.isFormOpen);
   const openForm = useTodoStore((state) => state.openForm);
+  const filterSetter = useTodoStore((state) => state.filterSetter);
 
   return (
     <main className=" min-h-screen flex justify-center bg-[#1A222E] ">
@@ -18,7 +18,7 @@ const TodoForm = () => {
         <div className="mt-6 sm:mt-12 flex justify-between  w-full px-12 ">
           <button
             onClick={openForm}
-            className="rounded-2xl bg-gray-600 px-3 py-2 cursor-pointer hover:scale-108 transition-all capitalize "
+            className="rounded-md bg-gray-600 px-3 py-2 cursor-pointer hover:scale-108 transition-all capitalize "
           >
             add task
           </button>
@@ -26,9 +26,21 @@ const TodoForm = () => {
           {/* AnimatePresence for exiting animation must be used in conditional rendered children */}
           <AnimatePresence>{isFormOpen && <DialogueBox />}</AnimatePresence>
 
-          <button className="rounded-2xl bg-gray-600 px-6 py-2 cursor-pointer hover:scale-108 transition-all">
-            All
-          </button>
+          <select
+            className="bg-gray-600 rounded-md p-2 px-4 "
+            onChange={(e) =>
+              filterSetter(e.target.value as "all" | "complete" | "incomplete")
+            }
+          >
+            <option value="All">All</option>
+            <option value="incomplete">Incomplete</option>
+            <option value="complete">Complete</option>
+          </select>
+        </div>
+
+        {/* Todos list are rendered by the component below */}
+        <div className="bg-black/20 w-full flex flex-wrap mt-4 p-4">
+          <TodoList />
         </div>
       </div>
     </main>
